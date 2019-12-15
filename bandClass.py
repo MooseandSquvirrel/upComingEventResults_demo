e3e1ee874e6780846c931406ec2df02db58a423b
MooseandSquvirrel <Andygardnerucla@gmail.com>
2019-12-15 00:48:54 -0800

    save for save sake

---
+++ bandClass.py
@@ -0,0 +1,7 @@
+class Bandaid:
+    def __init__(self, apiResult, postsCount, adminCount, commentCount):
+        self.apiResult = apiResult
+        self.postsCount = postsCount
+        self.adminCount = adminCount
+        self.commentCount = commentCount
+
---
+++ tester.sh
@@ -0,0 +1,2 @@
+
+printf 'v\ny\n1\ny\n' | python3 newevents.py
