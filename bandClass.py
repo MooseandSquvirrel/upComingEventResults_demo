6c172fc47885fe8c29209e645f69788363750fa3
MooseandSquvirrel <Andygardnerucla@gmail.com>
2019-12-16 02:24:39 -0800

    working with tokens

--- bandClass.py
+++ bandClass.py
@@ -1,7 +1,19 @@
 class Bandaid:
-    def __init__(self, apiResult, postsCount, adminCount, commentCount):
+    def __init__(self, apiResult, postsCount, adminCount, commentCount, memberCount):
         self.apiResult = apiResult
         self.postsCount = postsCount
         self.adminCount = adminCount
         self.commentCount = commentCount
+        self.memberCount = memberCount
+    
+    def get_totalActivity(self):
+        return self.__x
 
+    def set_totalActivity(self, x):
+        self.__x = x
+
+    def get_adminActivity(self):
+        return self.__x
+
+    def set_adminActivity(self, x):
+        self.__x = x
--- tester.sh
+++ tester.sh
@@ -1,2 +1,2 @@
 
-printf 'v\ny\n1\ny\n' | python3 newevents.py
+printf 'v\ny\n1\ny\n2\ny\n' | python3 newevents.py
